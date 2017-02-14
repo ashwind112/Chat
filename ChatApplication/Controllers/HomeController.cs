5d06cab234a4d4f5b1b804bbc8acabf3a87e68bd
ashwind112 <ashwind112@gmail.com>
2017-02-14 23:47:29 +0530

    added ui

---
+++ ChatApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChatApplication.Models;
+namespace ChatApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            User newUser = new User();
+            return View();
+        }
+    }
+}
